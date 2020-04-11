df3e4376a62af06d245f4232fc2aa7c9b05cd0dd
Raihan Yudo Saputra <raihanyudosaputra30@gmail.com>
2020-04-11 08:30:18 +0700

    add form validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,18 @@ var form = document.getElementById("claim-trial");
 
 function validateForm(e) {
   e.preventDefault();
-  let fields = document.getElementsByTagName("input");
-  console.log(fields);
+  let fields = Array.from(document.getElementsByTagName("input"));
+  let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  fields.forEach((e) => {
+    if (e.value.length <= 6) {
+      document.getElementById("error-msg").innerHTML =
+        "Fields are Empty (min 6 characters)";
+      e.classList.add("invalid");
+    } else {
+      e.classList.remove("invalid");
+    }
+  });
+  console.log(re.test(String("raihan2gmail.com").toLowerCase()));
 }
 
 form.addEventListener("submit", validateForm);
